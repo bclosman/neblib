816b5d79b428c0c4a3bdb5af1012aff2d4354e72
bclosman <bryceclosman@gmail.com>
2025-03-27 12:18:59 -0500

    Updating PID to use structs for exit conditions of multiple types. Needs PID functionality added

--- include/neblib/pid.hpp
+++ include/neblib/pid.hpp
@@ -5,121 +5,66 @@
 #include <math.h>
 
 namespace neblib {
-/**
- * @brief Struct to hold PID gains
- * 
- * @param kP proportional gain
- * @param kI integral gain
- * @param kD derivative gain
- */
-struct Gains {
-    double kP;
-    double kI;
-    double kD;
 
-    /**
-     * @brief Constructor for Gains struct
-     * 
-     * @param kP proportional gain
-     * @param kI integral gain
-     * @param kD derivative gain
-     */
-    Gains(double kP, double kI, double kD);
-};
-
+/// @brief PID Controller
 class PID {
-private:
-    Gains m_gains;
-
-    double m_windupRange;
-    double m_settleTolerance;
-    int m_settleTime;
-    int m_iterationDelay;
-    bool m_signFlipReset;
-
-    double m_previousError = 0;
-    double m_integral = 0;
-    int m_timeSettled = 0;
-    
 public:
-    /**
-     * @brief Creates a PID object
-     * 
-     * @param kP proportional gain
-     * @param kI integral gain
-     * @param kD derivative gain
-     * @param windupRange range that the integral value will "windup"
-     * @param settleTolerance range where the PID will be considered "settled"
-     * @param settleTime the length of time (in milliseconds) that the error needs to be within the settle tolerance
-     * @param iterationDelay the length of time (in milliseconds) that your loop's delay is
-     * @param signFlipReset if true, integral value will reset as error changes sign
-     */
-    PID(double kP, double kI, double kD, double windupRange, double settleTolerance, int settleTime, int iterationDelay, bool signFlipReset = false);
 
-    /**
-     * @brief Creates a PID object
-     * 
-     * @param gains Gains struct containing the gain values
-     * @param windupRange range that the integral value will "windup"
-     * @param settleTolerance range where the PID will be considered "settled"
-     * @param settleTime the length of time (in milliseconds) that the error needs to be within the settle tolerance
-     * @param iterationDelay the length of time (in milliseconds) that your loop's delay is
-     * @param signFlipReset if true, integral value will reset as error changes sign
-     */
-    PID(const Gains& gains, double windupRange, double settleTolerance, int settleTime, int iterationDelay, bool signFlipReset = false);
+    /// @brief Gains (P, I, D) for PID object
+    struct Gains {
+        double kP;
+        double kI;
+        double kD;
+        double windupRange;
 
-    /**
-     * @brief Sets the gains of the PID object
-     * 
-     * @param gains a Gains object
-     */
-    void setGains(const Gains& gains);
 
-    /**
-     * @brief Sets the gains of the PID object
-     * 
-     * @param kP proportional gain
-     * @param kI integral gain
-     * @param kD derivative gain
-     */
-    void setGains(double kP, double kI, double kD);
+        /// @brief Constructor for Gains struct
+        /// @param kP proportional constant
+        /// @param kI integral constant
+        /// @param kD derivative constant 
+        /// @param windupRange range where integration takes place
+        Gains(const double kP, const double kI, const double kD, const double windupRange = INFINITY);
+    };
 
-    /**
-     * @brief Sets the constants for the PID object
-     * 
-     * @param windupRange range that the integral value will "windup"
-     * @param settleTolerance range where the PID will be considered "settled"
-     * @param settleTime the length of time (in milliseconds) that the error needs to be within the settle tolerance
-     * @param iterationDelay the length of time (in milliseconds) that your loop's delay is
-     * @param signFlipReset if true, integral value will reset as error changes sign
-     */
-    void setConstants(double windupRange, double settleTolerance, int settleTime, int iterationDelay, bool signFlipReset = false);
+    /// @brief Exit conditions for PID object
+    struct ExitConditions {
+        /// @brief Type of exit condition used
+        enum Type {
+            SETTLE_TIME,
+            DERIVATIVE
+        };
 
-    /**
-     * @brief Resets the integral and deriative values to 0
-     */
-    void reset();
+        Type type;
+        double tolerance;
+        int timeStep;
 
-    /**
-     * @brief Updates the output value of the PID
-     * 
-     * @param error the error value
-     */
-    double update(double error);
+        /// @brief Struct for the settle time method
+        struct {
+            int settleTime;
+            int timeSettled = 0;
+        } SettleTime;
 
-    /**
-     * @brief Updates the output value of the PID
-     * 
-     * @param error the error value
-     * @param minOutput the minimum expected output value
-     * @param maxOutput the maximum expected outout value
-     */
-    double update(double error, double minOutput, double maxOutput);
+        /// @brief Struct for the derivative or velocity based method
+        struct {
+            double tolerance;
+        } Derivative;
 
-    /**
-     * @brief Returns true if settled, false otherwise
-     */
-    bool isSettled();
+        /// @brief Constructor for ExitConditions struct
+        /// @param type the type of exit conditions used
+        /// @param tolerance the error tolerance
+        /// @param timeStep the cycle time of the loop
+        /// @param requirement the derivative tolerance or required settle time
+        ExitConditions(const Type type, const double tolerance, const int timeStep, const double requirement);
+
+        /// @brief Updates and determines if the PID has settled
+        /// @param error the PID's error
+        /// @param derivative the current derivative value of the PID
+        /// @return true if settled, false otherwise
+        bool isSettled(const double error, const double derivative);
+    };
+
+private:
+
 };
 
 }
--- src/neblib/pid.cpp
+++ src/neblib/pid.cpp
@@ -1,60 +1,24 @@
 #include "neblib/pid.hpp"
 
-neblib::Gains::Gains(double kP, double kI, double kD) :
+neblib::PID::Gains::Gains(const double kP, const double kI, const double kD, const double windupRange) :
     kP(kP),
     kI(kI),
-    kD(kD) {}
+    kD(kD),
+    windupRange(windupRange) {}
 
-neblib::PID::PID(double kP, double kI, double kD, double windupRange, double settleTolerance, int settleTime, int iterationDelay, bool signFlipReset) :
-    m_gains(Gains(kP, kI, kD)),
-    m_windupRange(windupRange),
-    m_settleTolerance(settleTolerance),
-    m_settleTime(settleTime),
-    m_iterationDelay(iterationDelay),
-    m_signFlipReset(signFlipReset) {}
+neblib::PID::ExitConditions::ExitConditions(const neblib::PID::ExitConditions::Type type, const double tolerance, const int timeStep, const double requirement) :
+    type(type),
+    tolerance(tolerance),
+    timeStep(timeStep) {
+        if (type == SETTLE_TIME) SettleTime.settleTime = int(requirement);
+        else Derivative.tolerance = requirement;
+    }
 
-neblib::PID::PID(const Gains& gains, double windupRange, double settleTolerance, int settleTime, int iterationDelay, bool signFlipReset) :
-    m_gains(gains),
-    m_windupRange(windupRange),
-    m_settleTolerance(settleTolerance),
-    m_settleTime(settleTime),
-    m_iterationDelay(iterationDelay),
-    m_signFlipReset(signFlipReset) {}
-
-void neblib::PID::setGains(const Gains& gains) { m_gains = gains; }
-
-void neblib::PID::setGains(double kP, double kI, double kD) { m_gains = Gains(kP, kI, kD); }
-
-void neblib::PID::reset()
-{
-    m_previousError = 0;
-    m_integral = 0;
-}
-
-double neblib::PID::update(double error)
-{
-    const double derivative = error - m_previousError;
-
-    m_integral += error;
-    if (sign(error) != sign(m_previousError) && m_signFlipReset) m_integral = 0;
-    if (abs(error) > m_windupRange) m_integral = 0;
-
-    if (abs(error) < m_settleTolerance) m_timeSettled += m_iterationDelay;
-    else m_timeSettled = 0;
-
-    return error * m_gains.kP + m_integral * m_gains.kI + derivative * m_gains.kD;
-}
-
-double neblib::PID::update(double error, double minOutput, double maxOutput)
-{
-    double output = update(error);
-
-    if (output > maxOutput) return maxOutput;
-    if (output < minOutput) return minOutput;
-    return output;
-}
-
-bool neblib::PID::isSettled()
-{
-    return (m_timeSettled >= m_settleTime);
+bool neblib::PID::ExitConditions::isSettled(const double error, const double derivative) {
+    if (type == SETTLE_TIME) {
+        if (abs(error) <= tolerance) SettleTime.timeSettled += timeStep;
+        return SettleTime.timeSettled >= SettleTime.settleTime;
+    } else {
+        return (abs(error) <= tolerance) && (abs(derivative) <= Derivative.tolerance);
+    }
 }

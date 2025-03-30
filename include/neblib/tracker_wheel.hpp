8b077af23d295e56cb820eccafd8577241f99d8a
bclosman <bryceclosman@gmail.com>
2025-03-30 10:59:08 -0500

    Finished tracker wheel class

--- include/neblib/tracker_wheel.hpp
+++ include/neblib/tracker_wheel.hpp
@@ -6,10 +6,10 @@ namespace neblib {
 
 class TrackerWheel {
 private:
-    const vex::rotation* rotation;
-    const vex::motor* motor;
-    const vex::motor_group* motorGroup;
-    const vex::encoder* encoder;
+    vex::rotation* rotation;
+    vex::motor* motor;
+    vex::motor_group* motorGroup;
+    vex::encoder* encoder;
     
     double distancePerDegree;
 
@@ -18,25 +18,25 @@ public:
     /// @param rotation a VEX V5 Rotation sensor
     /// @param wheelDiameter diameter of the wheel
     /// @param ratio ratio, generally sensor gear teeth / wheel sensor teeth
-    TrackerWheel(const vex::rotation& rotation, const double wheelDiameter, const double ratio);
+    TrackerWheel(vex::rotation& rotation, const double wheelDiameter, const double ratio);
 
     /// @brief Creates a TrackerWheel object using a VEX V5 Motor
     /// @param motor a VEX V5 Motor
     /// @param wheelDiameter diameter of the wheel
     /// @param ratio ratio, generally sensor gear teeth / wheel sensor teeth
-    TrackerWheel(const vex::motor& motor, const double wheelDiameter, const double ratio);
+    TrackerWheel(vex::motor& motor, const double wheelDiameter, const double ratio);
 
     /// @brief Creates a TrackerWheel object using a VEX V5 Motor group
     /// @param motorGroup a VEX V5 Motor group
     /// @param wheelDiameter diameter of the wheel
     /// @param ratio ratio, generally sensor gear teeth / wheel sensor teeth
-    TrackerWheel(const vex::motor_group& motorGroup, const double wheelDiameter, const double ratio);
+    TrackerWheel(vex::motor_group& motorGroup, const double wheelDiameter, const double ratio);
 
     /// @brief Creates a TrackerWheel object using a VEX Legacy OS Encoder
     /// @param encoder a VEX Legacy OS Encoder
     /// @param wheelDiameter diameter of the wheel
     /// @param ratio ratio, generally sensor gear teeth / wheel sensor teeth
-    TrackerWheel(const vex::encoder& encoder, const double wheelDiameter, const double ratio);
+    TrackerWheel(vex::encoder& encoder, const double wheelDiameter, const double ratio);
 
 
     /// @brief Gets the distance of the tracker wheel
--- src/neblib/tracker_wheel.cpp
+++ src/neblib/tracker_wheel.cpp
@@ -1,29 +1,38 @@
 #include "neblib/tracker_wheel.hpp"
 
-neblib::TrackerWheel::TrackerWheel(const vex::rotation& rotation, const double wheelDiameter, const double ratio) :
+neblib::TrackerWheel::TrackerWheel(vex::rotation& rotation, const double wheelDiameter, const double ratio) :
     rotation(&rotation),
     motor(nullptr),
     motorGroup(nullptr),
     encoder(nullptr),
     distancePerDegree((ratio * (3.1415926535 * wheelDiameter)) / 360) {}
 
-neblib::TrackerWheel::TrackerWheel(const vex::motor& motor, const double wheelDiameter, const double ratio) :
+neblib::TrackerWheel::TrackerWheel(vex::motor& motor, const double wheelDiameter, const double ratio) :
     rotation(nullptr),
     motor(&motor),
     motorGroup(nullptr),
     encoder(nullptr),
     distancePerDegree((ratio * (3.1415926535 * wheelDiameter)) / 360) {}
 
-neblib::TrackerWheel::TrackerWheel(const vex::motor_group& motorGroup, const double wheelDiameter, const double ratio) :
+neblib::TrackerWheel::TrackerWheel(vex::motor_group& motorGroup, const double wheelDiameter, const double ratio) :
     rotation(nullptr),
     motor(nullptr),
     motorGroup(&motorGroup),
     encoder(nullptr),
     distancePerDegree((ratio * (3.1415926535 * wheelDiameter)) / 360) {}
 
-neblib::TrackerWheel::TrackerWheel(const vex::encoder& encoder, const double wheelDiameter, const double ratio) :
+neblib::TrackerWheel::TrackerWheel(vex::encoder& encoder, const double wheelDiameter, const double ratio) :
     rotation(nullptr),
     motor(nullptr),
     motorGroup(nullptr),
     encoder(&encoder),
     distancePerDegree((ratio * (3.1415926535 * wheelDiameter)) / 360) {}
+
+double neblib::TrackerWheel::getDistance() {
+    double position = 0;
+    if (rotation) position = rotation->position(vex::rotationUnits::deg);
+    else if (motor) position = motor->position(vex::rotationUnits::deg);
+    else if (motorGroup) position = motorGroup->position(vex::rotationUnits::deg);
+    else if (encoder) position = encoder->position(vex::rotationUnits::deg);
+    return position * distancePerDegree;
+}
